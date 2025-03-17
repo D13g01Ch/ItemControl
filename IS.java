ca47365831ddab18f228234825aa2287ff98cd28
ALELLUVIA <Perezlluvia730@gmail.com>
2025-03-16 23:14:47 -0600

    Add files via upload

---
+++ IS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.is;
+
+/**
+ *
+ * @author Lluvia Alejandra
+ */
+public class IS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
